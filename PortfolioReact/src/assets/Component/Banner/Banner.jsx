d470163d6ae28c644a6a1860d5205da843255556
Leandro Lopez <leanlopezmartin@gmail.com>
2024-07-30 17:00:08 -0300

    Responsive Banner. 2/10

--- PortfolioReact/src/assets/Component/Banner/Banner.jsx
+++ PortfolioReact/src/assets/Component/Banner/Banner.jsx
@@ -2,16 +2,17 @@ import { Flex, Text, Image } from '@chakra-ui/react'
 import dividerStar from "./img/4PointStar.svg"
 
 function Banner() {
-    const textStyle = {color:"rgb(230,230,230)", fontSize:"32px", fontWeight:"bold"}
+    const textStyle = {color:"rgb(230,230,230)", fontSize:{base:"22px", md:"22px", xl:"32px"}, fontWeight:"bold", textAlign:"center"}
+    const starStyle = {w:{md:"40px", lg:"35px", xl:"50px"}, display:{base:"none", md:"block"}}
   
     return (
-        <Flex as={'section'} w={"100vw"} justifyContent={"space-around"} alignItems={"center"} h={"150px"} bg={"rgb(232, 106, 66)"} px={"5vw"}>
+        <Flex as={'section'} w={"100vw"} justifyContent={"space-around"} alignItems={"center"} bg={"rgb(232, 106, 66)"} px={"5vw"} direction={{base:"column", md:"row"}} py={"20px"}>
             <Text sx={textStyle}>Diseño Web</Text>
-            <Image src={dividerStar} alt='Star divider' w={"50px"}></Image>
+            <Image src={dividerStar} alt='Star divider' sx={starStyle}></Image>
             <Text sx={textStyle}>Investigación de usuario</Text>
-            <Image src={dividerStar} alt='Star divider' w={"50px"}></Image>
+            <Image src={dividerStar} alt='Star divider' sx={starStyle}></Image>
             <Text sx={textStyle}>Soluciones móviles</Text>
-            <Image src={dividerStar} alt='Star divider' w={"50px"}></Image>
+            <Image src={dividerStar} alt='Star divider' sx={starStyle}></Image>
             <Text sx={textStyle}>Soluciones UI/UX</Text>
         </Flex>
     )
